70d98d46f09428f772e4c6fb9573e20f5a288465
khoa <dknguyen176@gmail.com>
2022-07-02 02:31:08 +0700

    Add Toolbar

--- app/src/main/java/com/example/senomerc/activities/MainActivity.java
+++ app/src/main/java/com/example/senomerc/activities/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.senomerc.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.bumptech.glide.load.engine.Initializable;
 import com.example.senomerc.R;
 import com.example.senomerc.fragments.HomeFragment;
 
@@ -13,12 +19,18 @@ public class MainActivity extends AppCompatActivity {
 
     Fragment homeFragment;
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportActionBar().hide();
+        toolbar = findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.menu2);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
         homeFragment = new HomeFragment();
         loadHomeFragment(homeFragment);
@@ -28,5 +40,22 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.home_container, homeFragment);
         transaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_cart) {
+            startActivity(new Intent(MainActivity.this, CartActivity.class));
+        }
+
+        return true;
     }
 }
